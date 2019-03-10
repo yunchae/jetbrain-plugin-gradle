13421ff7d760a0405116236543c717dd4a7a2cd1
yunchae <ck2204@naver.com>
2019-03-10 15:53:02 +0900

    [Action 변경] - Hello 버튼 클릭시 Dialog 표시되도록 변경

--- src/main/java/HelloAction.java
+++ src/main/java/HelloAction.java
@@ -1,6 +1,11 @@
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.DialogWrapper;
+
+import javax.annotation.Nullable;
+import javax.swing.*;
+import java.awt.*;
 
 public class HelloAction extends AnAction {
     public HelloAction() {
@@ -8,7 +13,31 @@ public class HelloAction extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent event) {
-        Project project = event.getProject();
-        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
+//        Project project = event.getProject();
+//        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
+        SampleDialogWrapper dialog = new SampleDialogWrapper();
+        dialog.pack();
+        dialog.show();
+    }
+
+    public class SampleDialogWrapper extends DialogWrapper {
+
+        public SampleDialogWrapper() {
+            super(true); // use current window as parent
+            init();
+            setTitle("Test DialogWrapper");
+        }
+
+        @Nullable
+        @Override
+        protected JComponent createCenterPanel() {
+            JPanel dialogPanel = new JPanel(new BorderLayout());
+
+            JLabel label = new JLabel("testing");
+            label.setPreferredSize(new Dimension(100, 100));
+            dialogPanel.add(label, BorderLayout.CENTER);
+
+            return dialogPanel;
+        }
     }
 }
